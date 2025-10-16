4174df4d897752004fd30213f5e0fa1932f08a6d
Sinchana-d <sinchanad87@gmail.com>
2025-10-16 10:32:31 +0530

    gopush

---
+++ react/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+    <Header /> { " "}
+    <div>
+      <h1>Hello From React</h1>
+      <p>Iam RACHANA from IOT</p>
+      </div>
+          </>
+
+  )
+  }
+export default App;
---
+++ react/src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+const Header = () => {
+    return (
+        <header>
+            <h1>This is my Header</h1>
+        </header>
+    )
+}
+export default Header;
